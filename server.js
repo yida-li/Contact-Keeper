558e73a824d9175f5b4603caa630d8d4c66e2cb5
Yida Li <gabrielyidali@gmail.com>
2020-02-28 20:33:04 -0500

    Prepare for deploy

---
+++ server.js
@@ -0,0 +1,38 @@
+// entry point to my backend!!!!
+
+// node as backend technology so so know
+
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+const path = require('path');
+//Connect Database
+connectDB();
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({
+    msg: 'Welcome to the contactkeeper api.....'
+  })
+); // res.send is sending data ok?
+
+//Define my routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000; // look for environment variable called port or 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
